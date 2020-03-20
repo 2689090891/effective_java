6874bd8d01ff9d922f32ac03aff67c7f9330c03b
admin <bjr3013>
2020-03-20 15:35:00 +0800

    Initial commit

---
+++ src/priv/bjr/effective/first/Demo.java
@@ -0,0 +1,38 @@
+package priv.bjr.effective.first;
+
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.sql.Date;
+import java.util.*;
+import java.util.regex.Pattern;
+
+/**
+ * @author bjr
+ * @create 2020/03/18
+ */
+public class Demo {
+
+    public enum Style {BOLD,ITALIC,UNDERLINE}
+
+    public static void main(String[] args) {
+
+        /*EnumSet<Style> bold = EnumSet.of(Style.BOLD, Style.UNDERLINE);
+
+        Object obj = new Object();
+        Objects.requireNonNull(obj);
+
+        String str = "";
+        str.matches("");
+
+        Pattern.compile("");
+
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        objectObjectHashMap.keySet();
+
+        WeakHashMap*/
+
+        System.out.println("------------");
+
+    }
+
+}
